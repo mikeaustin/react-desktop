85c92d33f4e4eed8e24b6f7d7b5767b12c0570be
Mike Austin <mike_ekim@yahoo.com>
2023-01-03 20:37:41 -0800

    Add code interpreter via MutationObserver

--- clients/kopi-ide/src/App2.tsx
+++ clients/kopi-ide/src/App2.tsx
@@ -9,6 +9,41 @@ import './App.css';
 function App() {
   const rootElementRef = useRef<HTMLDivElement>(null);
 
+  const handleChange = async (mutations: MutationRecord[]) => {
+    console.log(mutations);
+
+    for (const mutation of mutations) {
+      if (mutation.target.parentNode?.nodeName === 'CODE') {
+        console.log('here', mutation.target.textContent);
+
+        if (mutation.target.textContent && mutation.target.parentNode.lastChild) {
+          try {
+            const value = await interpret(mutation.target.textContent);
+
+            mutation.target.parentNode.lastChild.textContent = await value.inspect();
+          } catch (error) {
+            mutation.target.parentNode.lastChild.textContent = (error as Error).message;
+          }
+        }
+      }
+    }
+  };
+
+  useLayoutEffect(() => {
+    const mutationObserver = new MutationObserver(handleChange);
+
+    if (rootElementRef.current) {
+      mutationObserver.observe(rootElementRef.current, {
+        subtree: true,
+        characterData: true,
+      });
+    }
+
+    return () => {
+      mutationObserver.disconnect();
+    };
+  }, []);
+
   const handleKeyDown = (event: React.KeyboardEvent) => {
     const selection = window.getSelection();
 
@@ -81,12 +116,21 @@ function App() {
   };
 
   useEffect(() => {
+    const mutationObserver = new MutationObserver(handleChange);
+
+    if (rootElementRef.current) {
+      mutationObserver.observe(rootElementRef.current, {
+        subtree: true,
+        characterData: true,
+      });
+    }
+
     if (rootElementRef.current) {
       rootElementRef.current.innerHTML =
-        '<h3>Heading 1</h3>' +
+        '<h3>Factorial</h3>' +
         '<p>Lorem ipsum dolor sit amet, <code class="inline">consectetur</code> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>' +
         '<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>' +
-        '<code>factorial (n) = match n (\n  0 => 1\n  n => n * factorial (n - 1)\n)<br /></code>' +
+        '<code>factorial (n) = match n (\n  0 => 1\n  n => n * factorial (n - 1)\n)\n\nfactorial 5<br /><div contenteditable="false">result</div><pre contenteditable="false">120</pre></code>' +
         '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>' +
         '<ul>' +
         '<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>' +
@@ -96,8 +140,13 @@ function App() {
         '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>' +
         '<h3>Heading 2</h3>' +
         '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit<br /></p>' +
+        `<code>1..10 | map '(toFixed 2)<br /><pre></pre></code>` +
         '<p><br /></p>';
     }
+
+    return () => {
+      mutationObserver.disconnect();
+    };
   }, []);
 
   return (
@@ -112,7 +161,7 @@ function App() {
         <Text fontWeight="bold" style={{ marginLeft: 16 }}>Heading 3</Text>
       </View>
       <Divider />
-      <View flex padding="large" style={{ padding: 32 }}>
+      <View flex padding="large" style={{ padding: 32, overflow: 'auto' }}>
         <Text fontSize="large" fontWeight="light">Basic Kopi Examples</Text>
         <Spacer size="xlarge" />
         <div autoCorrect="false" spellCheck="false" ref={rootElementRef} contentEditable className="editor" onKeyDown={handleKeyDown} />
